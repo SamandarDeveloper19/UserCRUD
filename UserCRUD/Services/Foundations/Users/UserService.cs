fe6d1d464d7984f888e5b72a8ecab77c9fc8fe50
SamandarDeveloper19 <mamadiyorovsamandar19@gmail.com>
2023-12-09 20:49:24 +0500

    CODE RUB: Add User

---
+++ UserCRUD/Services/Foundations/Users/UserService.cs
@@ -0,0 +1,18 @@
+﻿using UserCRUD.Brokers.Storages;
+using UserCRUD.Models.Users;
+
+namespace UserCRUD.Services.Foundations.Users
+{
+    public class UserService : IUserService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public UserService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public User AddUser(User user) =>
+            this.storageBroker.InsertUser(user);
+    }
+}
